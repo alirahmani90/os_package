7f163d40e5f5a621c4f0b25808e38da3e3ce2a06
ali rahmani <alirahmanimohammad@gmail.com>
2023-02-13 00:59:13 +0330

    Add files via upload

---
+++ code/os.py
@@ -0,0 +1,27 @@
+import os
+
+os.system("cls")
+
+print('hello world')
+
+print(os.getcwd())
+
+print(os.listdir()[0])
+
+# os.mkdir('1/456')
+
+# os.makedirs('1/456/8')
+
+# os.rmdir('m')
+
+# os.remove('1234.py')
+
+# os.F_OK – Found
+# os.R_OK – Readable
+# os.W_OK – Writable
+# os.X_OK – Executable
+
+print(os.access('ducrthtyjyetjetk',os.F_OK))
+
+
+os.system("   shutdown /s /t 0")
